a890d099f348f8d4fe7c33c9adbd565e098ab360
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-12-05 12:55:13 +0100

    [e2e] Add performance test

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -6,4 +6,23 @@ describe('flight app', () => {
   it('visits the initial page and check th title', () => {
     cy.contains('Hello World!');
   });
+
+  it('should load inital page below 1 second', () => {
+    cy.visit('/', {
+      onBeforeLoad: (win) => {
+        win.performance.mark('start-loading');
+      },
+      onLoad: (win) => {
+        win.performance.mark('end-loading');
+      },
+    })
+      .its('performance')
+      .then((perf) => {
+        perf.measure('pageLoad', 'start-loading', 'end-loading');
+        const measure = perf.getEntriesByName('pageLoad')[0];
+        const duration = Math.round(measure.duration);
+        cy.log(`Page load duration: ${duration}`);
+        expect(duration).to.be.most(1000);
+      });
+  });
 });
