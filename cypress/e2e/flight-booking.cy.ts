3283891e7624a669ca5574ca8b30be27ba3997fb
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-12-05 13:58:00 +0100

    [e2e] Mock the Flight Search

--- cypress/e2e/flight-booking.cy.ts
+++ cypress/e2e/flight-booking.cy.ts
@@ -14,4 +14,13 @@ describe('flight booking feature', () => {
     cy.get('form .btn').first().click();
     cy.get('app-flight-card').its('length').should('be.gte', 3);
   });
+
+  it('should search for flights from Wien to Eisenstadt by intercepting the network', () => {
+    cy.fixture('flights').then((flights) => cy.intercept('GET', 'http://www.angular.at/api/flight**', flights));
+    cy.contains('a', 'Flights').click();
+    cy.get('input[name=from]').clear().type('Wien');
+    cy.get('input[name=to]').clear().type('Eisenstadt');
+    cy.get('form .btn').first().click();
+    cy.get('app-flight-card').should('have.length', 3);
+  });
 });
